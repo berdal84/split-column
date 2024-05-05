0f5d571795ecbcd08f9eae226f3af433a3080c78
Bérenger Dalle-Cort <berenger@42borgata.com>
2024-05-04 20:08:15 -0400

    feat: handle multiple columns
    ex: python -m main -i file.csv -c col1,col2

--- main_test.py
+++ main_test.py
@@ -1,3 +1,4 @@
+import os
 from src.App import App
 import filecmp
 
@@ -9,29 +10,39 @@ CSV_EXPECTED = f"{CSV_PYTEST_PATH}/expected"
 app = App()
     
 def test_01_single_column():
-    split_and_compare("test_01_single_column.csv")
+    split_and_compare("test_01_single_column.csv", "version")
+    split_and_compare("test_01_single_column.csv", "version", use_flags=True)
 
 def test_02_two_columns():
-    split_and_compare("test_02_two_columns.csv" )
+    split_and_compare("test_02_two_columns.csv", "title,version")
+    split_and_compare("test_02_two_columns.csv", "title,version", use_flags=True)
 
-def split_and_compare(test_file_name: str):
+def split_and_compare(
+        test_file_name: str,
+        columns: str,
+        use_flags: bool = False
+    ):
 
     input=f"{CSV_INPUT}/{test_file_name}"
     output=f"{CSV_OUTPUT}/{test_file_name}"
     expected=f"{CSV_EXPECTED}/{test_file_name}"
     
-    # First we try using long argument names
-    app.init(["--input", input, "--output", output, "--column", "version"])
-    app.run()
+    # Ensure an old output is not present (a failing test would succeed then!)
+    try:
+        os.remove(output)
+    except FileNotFoundError:
+        pass
 
-    same = filecmp.cmp(output, expected, shallow=False)
-    if not same:
-        raise Exception(f"Files {output} and {expected} differs")
+    # Run
+    if use_flags:
+        args = ["-i", input, "-o", output, "-c", columns]
+    else:
+        args = ["--input", input, "--output", output, "--columns", columns]
     
-    # Then with short argument names
-    app.init(["-i", input, "-o", output, "-c", "version"])
+    app.init(args)
     app.run()
 
+    # Compare the output with an expected file (hand-written one).
     same = filecmp.cmp(output, expected, shallow=False)
     if not same:
         raise Exception(f"Files {output} and {expected} differs")
--- src/App.py
+++ src/App.py
@@ -1,12 +1,19 @@
-import sys
 import pandas as pd
 import argparse
 
 class App:
     def __init__(self, override_args: list[str]|None = None):
         self.is_initialized = False
+        self.FIRST_ROW_ID = 1 # Excel is 1-based
+
         # argument_default=argparse.SUPPRESS to allow parsing programmatic arguments
-        self.parser = argparse.ArgumentParser("python main.py", argument_default=argparse.SUPPRESS)
+        PROG = "python -m main"
+        self.parser = argparse.ArgumentParser(
+            argument_default=argparse.SUPPRESS,
+            prog=PROG,
+            usage=f"{PROG} -m --input \"path/to/file.csv\" --columns \"title,version\"",
+        )
+
         self.args: any = None
         self.parser.add_argument(
             '-i',
@@ -15,26 +22,24 @@ class App:
             required=True,
             help="Path to a CSV file (absolute or relative)",
             dest="input"
-            )
+        )
         
         self.parser.add_argument(
             '-o',
             '--output',
             type=str,
-            default=None,
-            required=False,
             help="Path to the output CSV file (absolute or relative)",
             dest="output"
-            )
+        )
         
         self.parser.add_argument(
             '-c',
             '--columns',
             type=str,
             required=True,
-            help="The column(s) name to split, coma separated values (in development)",
+            help="The column(s) name to split, coma separated values (ex: title,author,version)",
             dest="columns"
-            )
+        )
         
         self.parser.add_argument(
             '-s'
@@ -42,17 +47,17 @@ class App:
             type=str,
             default=";",
             help="The character to use to split a cell",
-            dest="separator"
-            )
+            dest="separator",
+        )
         
         self.parser.add_argument(
             '-m',
             '--metadata-columns',
-            type=str,
+            type=bool,
             default=False,
             help="When this argument is present, two extra columns (id, original_id) will be added to have a reference between the new and the old CSV in terms of row number",
             dest="insert_metadata",
-            )
+        )
 
     def init(self, override_args: list[str]|None = None):
         self.args = self.parser.parse_args(override_args)
@@ -65,12 +70,12 @@ class App:
 
         print("Split ..")
 
-        # Configuration
+        # Determine in/out file path
         input_file_path= self.args.input
         output_file_path=self.args.output if self.args.output != None else input_file_path.replace(".csv", "_output.csv")
-        base_id = 1 # Excel is 1-based
 
         # Load CSV as a pandas DataFrame
+        #-------------------------------
         print("Read csv ..")
         if not input_file_path.endswith(".csv"):
             raise Exception("Input's file path must be a CSV file (ending in '.csv')")
@@ -78,42 +83,91 @@ class App:
         dataframe = pd.read_csv(file)
 
         # Split rows
+        #-----------
+
         print("Transform data ..")
-        data_split = dict()
+        # A dictionary to read from
+        in_buffer = dataframe.to_dict("index")
+        # A dictionary to write to
+        out_buffer: dict|None = None # To store the result of the last pass
         next_id = 0
-        column = self.args.columns.split(",")[0] # Only one column is handled
-        for [id, original_row] in dataframe.to_dict("index").items():
+        columns = self.args.columns.split(",")
 
-            # Split the column            
-            value: str = original_row[column]
-            split_values = value.split(self.args.separator)
+        # Insert "original_id" column to be able to find the source data from the split CSV.
+        if self.args.insert_metadata:
+            for [id, input_row] in in_buffer.items():
+                input_row['original_id'] = self.FIRST_ROW_ID + id
 
-            for each_split_value in split_values:
+        # Split each column specified by the user
+        # This could be optimized by identifying how many columns needs to be split for a given line
+        for column in columns:
+            print(f"Processing for column: {column} ..")
 
-                # Copy original values
-                new_values = dict(original_row)
+            # Swap in/out buffer, so the last result is now the input
+            if out_buffer is not None:
+                in_buffer = out_buffer
 
-                # Overwrite cell with a single value
-                new_values[column] = each_split_value
+            # Ensure out buffer is empty      
+            out_buffer = dict()
 
-                if self.args.insert_metadata:
-                    # Add extra columns
-                    new_values['original_id'] = base_id + id # this is the line id from the source csv
-                    new_values['new_id'] = base_id + next_id # this is the current line id from the split csv
+            # Process each rows from the input buffer
+            for [id, input_row] in in_buffer.items():
+                print(f"Processing row: #{id} ..")
+                last_id = self.split_cell_at_column_and_distribute_into_rows(
+                    input_row=input_row,
+                    column_to_split=column,   
+                    output=out_buffer,
+                    output_id=next_id,                                                       
+                )
+                next_id = last_id + 1
 
-                # Uncomment this line if you want to see output in the console while running script
-                # print(new_values)
-
-                # Append the new values to the split data
-                data_split[next_id] = new_values
-
-                # Increment to give a new id to the next new value
-                next_id += 1
+        # Last temp data IS not the result!
+        output_data = out_buffer            
 
         # Write data split to a CSV file
         print("Write output .. ")
         with open(output_file_path, "w") as file_split:
-            dataframe_split = pd.DataFrame.from_dict(data_split, 'index')
+            dataframe_split = pd.DataFrame.from_dict(output_data, 'index')
             dataframe_split.to_csv(file_split, mode = 'w', index=False)
 
         print('Split DONE, see output file: "{}"'.format(output_file_path))
+
+
+    def split_cell_at_column_and_distribute_into_rows(
+            self,
+            input_row: dict,
+            column_to_split: str,           
+            output: dict,   
+            output_id: int,                        
+        ) -> int :
+        
+        """
+        Takes a row and duplicate it to spread each values found in a given column.
+
+        Ex:
+            title, version   ---- becomes ---->  title, version, [id], [original_id]
+            Alien, 1;2;                          Alien, 1      , 1   , 1        
+                                                 Alien, 2      , 2   , 1
+        """
+
+        # Split the cell at given column            
+        cell_value_to_split: str = input_row[column_to_split]
+        split_values = cell_value_to_split.split(self.args.separator)
+
+        if len(split_values) == 1:
+            output[output_id] = input_row # by ref
+            output_id += 1
+        else:
+            print(f"Found multiple values in column '{column_to_split}': '{cell_value_to_split}'")
+            for each_split_value in split_values:
+                print(f"-- making row for: '{each_split_value}'")
+
+                # Copy original values
+                new_values = dict(input_row) # by copy
+                # Overwrite cell with a single value
+                new_values[column_to_split] = each_split_value
+
+                output[output_id] = new_values
+                output_id += 1                
+
+        return output_id
